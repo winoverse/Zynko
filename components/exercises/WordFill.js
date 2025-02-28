d637c5bf1bcb105b627f3f6679b61d4664f6edd6
winoverse <Winoverse2025@gmail.com>
2025-02-28 17:42:03 +0530

    Update WordFill.js

--- components/exercises/WordFill.js
+++ components/exercises/WordFill.js
@@ -1,35 +1,41 @@
 import React, { useState } from 'react';
 
-const WordFill = ({ questions }) => {
+function WordFill({ questions }) {
     const [answers, setAnswers] = useState({});
 
-    return (
-        <div className="word-fill-container">
-            <h3>Fill in the blanks</h3>
-            {questions.map((q, index) => (
-                <div key={index} className="word-fill-question">
-                    {q.sentence.split('___').map((part, i, arr) => (
-                        <React.Fragment key={i}>
-                            {part}
-                            {i < arr.length - 1 && (
-                                <span className="word-options">
-                                    {q.options.map(option => (
-                                        <button
-                                            key={option}
-                                            onClick={() => setAnswers({...answers, [index]: option})}
-                                            className={answers[index] === option ? 'selected' : ''}
-                                        >
-                                            {option}
-                                        </button>
-                                    ))}
-                                </span>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </div>
-            ))}
-        </div>
+    return React.createElement(
+        'div',
+        { className: 'word-fill-container' },
+        React.createElement('h3', null, 'Fill in the blanks'),
+        questions.map((q, index) => 
+            React.createElement(
+                'div',
+                { key: index, className: 'word-fill-question' },
+                q.sentence.split('___').map((part, i, arr) => 
+                    React.createElement(
+                        React.Fragment,
+                        { key: i },
+                        part,
+                        i < arr.length - 1 && React.createElement(
+                            'span',
+                            { className: 'word-options' },
+                            q.options.map(option => 
+                                React.createElement(
+                                    'button',
+                                    {
+                                        key: option,
+                                        onClick: () => setAnswers({...answers, [index]: option}),
+                                        className: answers[index] === option ? 'selected' : ''
+                                    },
+                                    option
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
     );
-};
+}
 
-export default WordFill; 
+window.WordFill = WordFill; 
